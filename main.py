d4981ca70de74c7cc40fca38bad73577b9853185
CHRIS_LEE <chrismlee87@gmail.com>
2021-03-05 20:16:56 -0800

    commit

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import random
 
 
+# Start Class: This is where the pizza maker starts and stores all the methods for user input options
 class Start():
+    # Instantiated Attributes
     def __init__(self, pizza_name, order_num=None, pizza_price=0):
         self.pizza_name = pizza_name
         if order_num is None:
             order_num = random.randint(1000, 9999)
         self.order_num = order_num
-        self.price = price
+        self.pizza_price = pizza_price
 
+    # Start pizza making app
     def Start_Pizza(self):
-        pizza_start = none
-        pizza_name = Pizza(name, size, sauce, crust)  # <------- wtf!!!!
+        pizza_start = None
+        # pizza_name = Pizza(name, size, sauce, crust)  # <------- wtf!!!!
+        # pizza_size <---------
+
         while pizza_start != '4':
             pizza_start = int(input(
-                'What Pizza do you want?\n[1] Full Pizza\n[2] Slice \n[3] Custom Pizza\n[4] Random'))
+                'What Pizza type do you want?\n[1] Full Pizza\n[2] Slice \n[3] Custom Pizza\n[4] Random'))
             if pizza_start == '1':
                 # Operate pizza class which has random pizzas to choose Class Pizza
+                self.create_pizza()
+                pizza_name.append(self.create_pizza)
                 pass
             elif pizza_start == '2':
                 # Operate slice class (same as pizza) Class Slice
@@ -26,21 +33,34 @@ class Start():
                 pass
             elif pizza_start == '4':
                 pass
-                # Return pizza
+
+            # return
 
     def create_pizza(self):
         # [1]
+        # if statements that allow choices
+        # instantiates classes to create next steps
+        pass
+
+    def pizza_size(self):
+        # if statements that allow choices
+        # instantiates classes to create next steps
         pass
 
     def create_slice(self):
+        # if statements that allow choices
+        # instantiates classes to create next steps
         # [2]
         pass
 
     def create_custom(self):
+        # if statements that allow choices
+        # instantiates classes to create next steps
         # [3]
         pass
 
     def random_pizza(self):
+        # Random goes straight to print_receipt. Could be anything from pizza list
         # [4]
         pass
 
@@ -48,43 +68,43 @@ class Start():
 class Pizza():
     def __init__(self, size, sauce, crust):
         super().__init__(self, pizza_price, order_num)
-        self.size = size  # s,m,l
+        self.toppings = list()
         self.sauce = sauce  # white or red
-        self.crust = crust
+        self.crust = ['thin', 'pan', 'cheese-crust']
         self.pizza_price = 1
+        self.size = ['small', 'medium', 'large']
 
-    def pizza_style(self):
-        # Chain - All, NYC - All, Chicago (Full Only), Slices - Slice Only
-        pizza_type = ['Chain', 'NYC', 'Chicago', 'Slices']
+        # Should be a list of pizzas in here to choose from
+        self.pizza_list = ['cheese', 'pepperoni', 'margherita', ]
 
     def pizza_size(self, size):
         self.pizza_price = 1
-        size = ['small', 'medium', 'large']
-        if size == 'small':
-            pizza_price = pizza_price * 0.75
-        if size == 'medium':
-            pizza_price = pizza_price
-        if size == 'large':
-            pizza_price = pizza_price * 1.75
-        pass
-        pass
+        self.size = ['small', 'medium', 'large']
+        if self.size == 'small':
+            pizza_price = 8
+        elif self.size == 'medium':
+            pizza_price = 12
+        elif self.size == 'large':
+            pizza_price = 18
+        return pizza_price
+
+# Slice inherits from pizza, ------> all same as pizza except return /8 <--------
 
 
 class Slice(Pizza):
-    def __init__(self,):
-
+    def __init__(self):
+        self.pizza_price = 1
+        self.size = 'large'
         # price from pizza class goes here, but this one /8
-        super().__init__(self, pizza_price, order_num)
-        pass
+        return pizza_price / 8
 
 
-class Custom_Pizza():
+class Custom_Pizza():  # ---------> inherits from <------
     def __init__(self, size, sauce, crust):
-        self.size = size  # s,m,l
+        self.size = size
         self.sauce = sauce  # white or red
         self.crust = crust
         self.pizza_price = 1
-        super().__init__(self, pizza_price, order_num)
 
     def create_crust(self, crust):
         self.crust = crust
@@ -94,10 +114,13 @@ class Custom_Pizza():
         pass
 
     def pick_sauce(self, sauce):
-        self.sauce.append(sauce)
+
         pass
 
-    def add_toppings(self, toppings):
+    def add_toppings(self, toppings):  # **kwargs <--------
+        for topping in self.toppings:
+            self.pizza_price.length * random.randint(2, 4)
+
         self.toppings = toppings
         self.toppings.append(topping)
         pass
@@ -125,27 +148,7 @@ class Bake():
     def pizza_return(self, **args):
         # Buzz buzz ,beep boop, your pizza is ready.
         print(
-            f'Buzz, Buzz, Beep, Boop. Your Pizza is ready\n{pizza_name}\nYou ordered a {pizza_type} with {crust}crust {sauce} and {toppings}.\n Please pay {pizza_price}')
+            f'Buzz, Buzz, Beep, Boop. Your Pizza is ready:\n{pizza_name}\nYou ordered a {pizza_size} pizza with {crust} crust {sauce} and {toppings}.\nYour total is: {pizza_price}')
 
-        # This class introduces the game, initializes everything
-        # welcome to pizza restaurant simulator
-        # This is how it works , etcetc.
 
-        # At least 4 classes are defined
-        # Each class has at least 2 attributes with __init__()
-        # Each class has at least 2 methods that use and or modify class attributes
-
-        # All classes are used to instantiate example objects
-
-        # Rationale about which attributes ad methods are made private, protected, or public is provided in code comments
-        # or verbally in presentation
-
-        # At least one class demonstrates composition (being composed of other objects)
-
-        # A diagram is provided that shows an overview of all the classes that make up the system design
-        # Diagram shows relationships between classes
-
-        # At least one class inherits from another class
-        # The Subclass overrides at least one superclass method
-
-        # 5-10 Minute Demo
+#      if __name__ == "__main__":
