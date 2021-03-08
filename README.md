7a518617747976d74bf9a631561bcea27e4e1cef
CHRIS_LEE <chrismlee87@gmail.com>
2021-03-08 15:39:41 -0800

    master

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# WELCONE
+# WELCOME
+
+![GitHub top language](https://img.shields.io/github/languages/top/chrismlee26/pizza-maker)
+![GitHub commit activity](https://img.shields.io/github/commit-activity/y/chrismlee26/pizza-maker)
+![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/chrismlee26/pizza-maker)
+
+- Welcome to my Pizza Maker OOP Project in Python for CS 1.1 (OOP) at Make School
+
+# Run
+
+- python3 main.py to run or hit the run button in VSCode
+
+# Pizza-maker
+
+- Pizza maker is a python project using OOP methods and user-inputs in terminal to create a pizza. This project involves encapsulation, composition, overriding, classes and methods and multiple inheritance.
+
+- A UML diagram is included as pizza-maker.pdf
--- main.py
+++ main.py
@@ -38,51 +38,6 @@ class Start():
             break
 
 
-class Pizza():  # This class is the parent class for pizzas
-    def __init__(self):
-        self.toppings = list()
-        self.size_options = {'small': 8, 'medium': 12, 'large': 18}
-        self.pizza_type = ['cheese', 'hawaiian']
-        self.pizza_price = 1
-        self.size = None
-        self.crust = None
-
-    # This method asks the user for size and returns a price in the size_options dictionary.
-    def pizza_size(self):
-        size_input = input(
-            'What size do you want?\n[1] Small \n[2] Medium \n[3] Large\n')
-        if size_input == '1':
-            self.size = 'small'
-        elif size_input == '2':
-            self.size = 'medium'
-        elif size_input == '3':
-            self.size = 'large'
-        # Assigning the price of the pizza based on the size
-        self.pizza_price = self.size_options[self.size]
-        return self.size
-
-    # This method asks the user for a crust type for the bake() return
-    def create_crust(self):
-        crust_input = input(
-            'What crust type?\n[1] Thin \n[2] Pan \n[3] Cheese-Crust\n')
-        if crust_input == '1':
-            self.crust = 'thin'
-        elif crust_input == '2':
-            self.crust = 'pan'
-        elif crust_input == '3':
-            self.crust = 'cheese-crust'
-        return self.crust
-
-    # This method asks user to select between pre-made pizza choices.
-    def choose_type(self):
-        type_input = input(
-            'What type of pizza do you want?\n[1] cheese \n[2] hawaiian\n')
-        if type_input == '1':
-            self.pizza_type = 'cheese'
-        elif type_input == '2':
-            self.pizza_type = 'hawaiian'
-        return self.pizza_type
-
 # Inherits from Pizza with new methods for customization of a pizza
 
 
