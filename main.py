d215e23d6854b3a0d2270f94ca167ade5c839569
CHRIS_LEE <chrismlee87@gmail.com>
2021-03-11 18:30:42 -0800

    bugfixes

--- main.py
+++ main.py
@@ -38,6 +38,55 @@ class Start():
             break
 
 
+class Pizza():  # This class is the parent class for pizzas
+    def __init__(self):
+        self.toppings = list()
+        self.size_options = {'small': 8, 'medium': 12, 'large': 18}
+        self.pizza_type = ['cheese', 'pepperoni']
+        self.pizza_price = 1
+        self.size = None
+        self.crust = None
+
+    # This method asks the user for size and returns a price in the size_options dictionary.
+    def pizza_size(self):
+        size_input = input(
+            'What size do you want?\n[1] Small \n[2] Medium \n[3] Large\n')
+        if size_input == '1':
+            self.size = 'small'
+        elif size_input == '2':
+            self.size = 'medium'
+        elif size_input == '3':
+            self.size = 'large'
+        # Assigning the price of the pizza base don the size
+        self.pizza_price = self.size_options[self.size]
+        return self.size
+
+    # This method asks the user for a crust type for the bake() return
+    def create_crust(self):
+        crust_input = input(
+            'What crust type?\n[1] Thin \n[2] Pan \n[3] Cheese-Crust\n')
+        if crust_input == '1':
+            self.crust = 'thin'
+        elif crust_input == '2':
+            self.crust = 'pan'
+        elif crust_input == '3':
+            self.crust = 'cheese-crust'
+        return self.crust
+
+    # This method asks user to select between pre-made pizza choices.
+    def choose_type(self):
+        type_input = input(
+            'What type of pizza do you want?\n[1] Cheese\n[2] Margherita\n[3] Pepperoni\n[4] Clam\n')
+        if type_input == '1':
+            self.pizza_type = 'Cheese'
+        elif type_input == '2':
+            self.pizza_type = 'Margherita'
+        elif type_input == '3':
+            self.pizza_type = 'Pepperoni'
+        elif type_input == '4':
+            self.pizza_type = 'Clam'
+        return self.pizza_type
+
 # Inherits from Pizza with new methods for customization of a pizza
 
 
